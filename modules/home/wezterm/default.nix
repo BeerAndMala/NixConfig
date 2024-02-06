695df84d294469b6dba0870a0c7c2d665420d57d
BeerAndMala <beer@ben.sh>
2024-02-05 22:35:55 -0500

    Add wezterm custom config

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -4,5 +4,12 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    extraConfig = ''
+      return {
+        font = wezterm.font("JetBrainsMono Nerd Font"),
+        hide_tab_bar_if_only_one_tab = true,
+      }
+    '';
   };
 }
