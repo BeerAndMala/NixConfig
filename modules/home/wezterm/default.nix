6c72689330cbee75935b8df118286b25b820bf5d
BeerAndMala <beer@ben.sh>
2024-02-08 11:58:50 +0800

    Theme wezterm

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -7,6 +7,7 @@
     extraConfig = ''
       return {
         font = wezterm.font("JetBrainsMono Nerd Font"),
+        color_scheme = "Catppuccin Mocha",
         font_size = 8,
         hide_tab_bar_if_only_one_tab = true,
       }
