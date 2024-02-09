4cb651a731154cd11d962afe678cc9a8c44ae0b4
BeerAndMala <beer@ben.sh>
2024-02-09 18:04:33 +0800

    btop theme

--- modules/home/btop/default.nix
+++ modules/home/btop/default.nix
@@ -1,11 +1,21 @@
-{pkgs, ...}: {
+{inputs, pkgs, ...}:
+let
+ theme =  pkgs.fetchFromGitHub{
+  owner = "catppuccin";
+  repo = "btop";
+  rev = "c646919";
+  hash = "sha256-jodJl4f2T9ViNqsY9fk8IV62CrpC5hy7WK3aRpu70Cs=";
+};
+in {
   programs.btop = {
     enable = true;
 
     settings = {
-      color_theme = "dracula";
+      color_theme = "catppuccin_mocha";
       theme_background = false;
       update_ms = 500;
     };
   };
+
+  home.file.".config/btop/themes/catppuccin_mocha.theme".source = "${theme}/themes/catppuccin_mocha.theme";
 }
