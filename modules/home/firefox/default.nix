6eec755416a767cd9846cb6f8e8b2da1db6a1b0c
BeerAndMala <beer@ben.sh>
2024-02-12 23:00:03 +0800

    Add Firefox work profile

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -9,7 +9,7 @@
     enable = true;
 
     profiles = {
-      bam = {
+      private = {
         id = 0;
         isDefault = true;
         name = "Private";
@@ -52,14 +52,82 @@
         settings = {
           # specify profile-specific preferences here; check about:config for options
           "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-          "browser.startup.homepage" = "https://nixos.org";
           "browser.newtabpage.pinned" = [
             {
               title = "NixOS";
               url = "https://nixos.org";
             }
           ];
-          # add preferences for profile_0 here...
+        };
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+          bitwarden
+          tree-style-tab # Set theme to photon imperatively?
+          i-dont-care-about-cookies
+        ];
+        extraConfig = builtins.readFile ./user.js;
+        userChrome = ''
+          #TabsToolbar {
+            visibility: collapse !important;
+          }
+
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            display: none;
+          }%
+        '';
+        userContent = ''
+          # Here too
+        '';
+      };
+      cyberowl = {
+        id = 1;
+        isDefault = false;
+        name = "CyberOwl";
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+            "NixOS Wiki" = {
+              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@nw"];
+            };
+            "Wikipedia (en)".metaData.alias = "@wiki";
+            "Google".metaData.hidden = true;
+            "Amazon.com".metaData.hidden = true;
+            "Bing".metaData.hidden = true;
+            "eBay".metaData.hidden = true;
+          };
+        };
+        settings = {
+          # specify profile-specific preferences here; check about:config for options
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "browser.newtabpage.pinned" = [
+            {
+              title = "NixOS";
+              url = "https://nixos.org";
+            }
+          ];
         };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
