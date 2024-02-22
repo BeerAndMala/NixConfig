0465719c4a8906ba870e00542bae19c12aaeceb9
BeerAndMala <beer@ben.sh>
2024-02-22 17:53:52 +0800

    Swap Discord for Webcord

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   imports = [(import ./theme-template.nix)];
   home.packages = with pkgs; [
-    (discord.override {
-      withVencord = true;
-    })
+    webcord-vencord
   ];
 }
