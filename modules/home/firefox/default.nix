afcfb2debdbe05427ed81c2aa90707b588d38f5b
BeerAndMala <beer@ben.sh>
2024-03-23 17:08:47 +0800

    Add Nxt4Sap Firefox profile

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -150,6 +150,77 @@
           # Here too
         '';
       };
-    };
+      nxt4sap = {
+        id = 2;
+        isDefault = false;
+        name = "Nxt4Sap";
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+            "NixOS Wiki" = {
+              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@nw"];
+            };
+            "Wikipedia (en)".metaData.alias = "@wiki";
+            "Google".metaData.hidden = true;
+            "Amazon.com".metaData.hidden = true;
+            "Bing".metaData.hidden = true;
+            "eBay".metaData.hidden = true;
+          };
+        };
+        settings = {
+          # specify profile-specific preferences here; check about:config for options
+          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+          "browser.newtabpage.pinned" = [
+            {
+              title = "NixOS";
+              url = "https://nixos.org";
+            }
+          ];
+        };
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+          bitwarden
+          tree-style-tab # Set theme to photon imperatively?
+          i-dont-care-about-cookies
+          firefox-color
+        ];
+        extraConfig = builtins.readFile ./user.js;
+        userChrome = ''
+          #TabsToolbar {
+            visibility: collapse !important;
+          }
+
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            display: none;
+          }%
+        '';
+        userContent = ''
+          # Here too
+        '';
+          };
+      };
   };
 }
